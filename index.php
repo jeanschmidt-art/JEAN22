5d55339e0a198f805f5397ad15d73b470b4614db
JEAN22 <jean.schmidt@aluno.senai.br>
2026-09-15 17:11:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,70 @@
 <?php
 
 $nome = "";
-$idade = 0;
+$idade = "";
 $resultado = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $nome = $_POST["nome"];
     $idade = $_POST["idade"];
 
     if ($idade >= 18) {
-        $resultado = "Você é maior de idade";
+        $resultado = "$nome, você é maior de idade.";
     } else {
-        $resultado = "Você é menor de idade";
+        $resultado = "$nome, você é menor de idade.";
     }
 }
 
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>JEAN22 aluno</title>
-    </head>
-    <body>
+<html lang="pt-br">
 
-        <form method="POST">
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome">
-            <label for="idade">Idade:</label>
-            <input type="number" id="idade" name="idade">
-            <button type="submit">Enviar</button>
-        </form> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <?php if($resultado != "") { ?>
-            <p><?= $resultado ?></p>
-        <?php } ?>
+    <title>Verificação de Idade</title>
+</head>
 
-    </body>
+<body>
+
+    <h1>Verificação de Idade</h1>
+
+    <form method="POST">
+
+        <label for="nome">Nome:</label>
+        <input
+            type="text"
+            id="nome"
+            name="nome"
+            required
+        >
+
+        <br><br>
+
+        <label for="idade">Idade:</label>
+        <input
+            type="number"
+            id="idade"
+            name="idade"
+            min="0"
+            required
+        >
+
+        <br><br>
+
+        <button type="submit">Enviar</button>
+
+    </form>
+
+    <?php if ($resultado != "") { ?>
+
+        <p><?= $resultado ?></p>
+
+    <?php } ?>
+
+</body>
+
 </html>
