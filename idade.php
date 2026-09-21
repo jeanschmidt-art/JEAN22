90c76191ea4bf61c668348c86779a419313b607e
jeanschmidt-art <jean.schmidt@aluno.senai.br>
2026-09-21 17:09:02 -0300

    .

--- idade.php
+++ idade.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="style.css">
     <title>Verificação de Idade</title>
 </head>
 
